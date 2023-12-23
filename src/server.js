284ecc1d454101d2bc5f35af6cf3d4c4c3c801c6
unknown <vdnam.ute@gmail.com>
2023-12-23 21:24:35 +0700

    update

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import initWebRoute from './route/web';
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 81;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +16,7 @@ configViewEngine(app);
 //init web route
 initWebRoute(app);
 
-app.listen(port,'192.168.1.11', () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`Example app listening at ${port}`)
 })
 
