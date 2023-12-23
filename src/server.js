472863c94754137248af88090c2a6249909b642d
unknown <vdnam.ute@gmail.com>
2023-12-23 22:36:23 +0700

    update

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import initWebRoute from './route/web';
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 81;
+const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
